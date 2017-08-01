da94dc588f090ab91d40003f1e20ac45fb191217
Gohar Mkrtchyan <g.mkrtchyan@apollobytes.com>
2017-08-02 00:34:37 +0400

    add searchbox to map

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {
-      lat: 4.444997,
-      lng: 106.554199
+      lat: 54,
+      lng: 108,
     },
-    zoom: 5
+    zoom: 4,
+  });
+
+  var input = document.getElementById('input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
   });
 
   map.addListener("click", function(data) {
@@ -12,4 +20,59 @@ function initAutocomplete() {
     var lon = data.latLng.lng();
     console.log(lat, lon);
   })
+
+  var markers = [];
+
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Place has no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(80, 60),
+        origin: new google.maps.Point(0, 2),
+        anchor: new google.maps.Point(21, 21 ),
+        scaledSize: new google.maps.Size(21, 21)
+      };
+
+      markers = new google.maps.Marker({
+        lat: place.geometry.location.lat(),
+        lon: place.geometry.location.lng(),
+        map: map,
+        icon: icon,
+        position: place.geometry.location,
+        title: place.name,
+      });
+
+      var infowindow = new google.maps.InfoWindow({
+        content: '<h1>' + markers["title"] + '</h1>' +
+        '<div class = "lat">' + "Lat: " + markers["lat"] + '</div>' +
+        '<div class = "lon">' + "Lon: " + markers["lon"] + '</div>'
+      });
+
+      markers.addListener('click', function() {
+        infowindow.open(map, markers);
+      });
+
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
 }
